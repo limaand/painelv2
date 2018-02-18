eecf57a23b7d7556b4d3194f54512a5215671264
Anderson Pereira <limaand@gmail.com>
2018-02-18 04:01:35 -0300

    apotando o banco mysql da locaweb

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,10 +3,15 @@ var mysql = require('mysql');
 var connMySQL = function(){
     console.log('conexao com BD estabelecida');
     return mysql.createConnection({
-        host:'localhost',
+        /*host:'localhost',
         user:'root',
         password:'',
-        database:'portalnode'
+        database:'portalnode'*/
+
+        host:'novaimagem1.mysql.dbaas.com.br',
+        user:'novaimagem1',
+        password:'heroku1020',
+        database:'novaimagem1'
     });
 }
 
