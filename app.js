b3d2073771d6b77a4dc1486e8e1466cd81fdf807
Anderson Pereira <limaand@gmail.com>
2018-02-18 14:13:28 -0300

    alterando package

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var app = require('./config/server');
 
 
-var porta = process.env.PORT || 8080;
+var porta = process.env.PORT || 5000;
 
 app.listen(porta, function(){
-  // console.log('http://localhost:3000'); 
-  // console.log('Servidor ON');
+   console.log('http://localhost:5000'); 
+   console.log('Servidor ON');
 });
