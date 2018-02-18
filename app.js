fb0a65fed74e86f2aee30a20ca21e24acc3f6495
Anderson Pereira <limaand@gmail.com>
2018-02-18 04:20:37 -0300

    alteracao no package.json e a criação de variavel no  app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var app = require('./config/server');
 
-app.listen(8080, function(){
+
+var porta = process.env.PORT || 8080;
+
+app.listen(porta, function(){
   // console.log('http://localhost:3000'); 
   // console.log('Servidor ON');
 });
